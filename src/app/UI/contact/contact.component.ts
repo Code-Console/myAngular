253eef8b44d6e1c42b4e0535d1fe54aa20b5bf5d
yogeshbangar <yogeshbangar@gmail.com>
2022-12-17 16:08:55 +0530

    Add contact forn

--- src/app/UI/contact/contact.component.ts
+++ src/app/UI/contact/contact.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { faSkype,faGithub,faLinkedin } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.sass']
 })
 export class ContactComponent {
-
+  faSkype=faSkype;
 }
