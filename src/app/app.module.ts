bed7cfacfa9e9924bb013cba529006f19393fbde
yogeshbangar <yogeshbangar@gmail.com>
2022-12-17 15:06:56 +0530

    Update paroote rotation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { MenuComponent } from './UI/menu/menu.component';
 import { HomeComponent } from './UI/home/home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Home3DComponent } from './Three/home3-d/home3-d.component';
+import { HeaderComponent } from './UI/header/header.component';
+import { ContactComponent } from './UI/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    Home3DComponent
+    Home3DComponent,
+    HeaderComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
